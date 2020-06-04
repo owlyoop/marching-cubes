af8a064f56147f47ae9485aea2a12e3cd3c965ae
owlyoop <44420599+owlyoop@users.noreply.github.com>
2020-06-04 16:37:31 -0500

    main upload

---
+++ Assets/FreeCam.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// A simple free camera to be added to a Unity game object.
+/// 
+/// Keys:
+///	wasd / arrows	- movement
+///	q/e 			- up/down (local space)
+///	r/f 			- up/down (world space)
+///	z/x             - dig/raise terrain
+///	pageup/pagedown	- up/down (world space)
+///	hold shift		- enable fast movement mode
+///	right mouse  	- enable free look
+///	mouse			- free look / rotation
+///
+///     
+/// </summary>
+public class FreeCam : MonoBehaviour
+{
+    /// <summary>
+    /// Normal speed of camera movement.
+    /// </summary>
+    public float movementSpeed = 10f;
+
+    /// <summary>
+    /// Speed of camera movement when shift is held down,
+    /// </summary>
+    public float fastMovementSpeed = 100f;
+
+    /// <summary>
+    /// Sensitivity for free look.
+    /// </summary>
+    public float freeLookSensitivity = 3f;
+
+    /// <summary>
+    /// Amount to zoom the camera when using the mouse wheel.
+    /// </summary>
+    public float zoomSensitivity = 10f;
+
+    /// <summary>
+    /// Amount to zoom the camera when using the mouse wheel (fast mode).
+    /// </summary>
+    public float fastZoomSensitivity = 50f;
+
+    /// <summary>
+    /// Set to true when free looking (on right mouse button).
+    /// </summary>
+    private bool looking = false;
+
+    Camera cam;
+    WorldGenerator world;
+
+    public GameObject debugCube;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        world = FindObjectOfType<WorldGenerator>();
+        debugCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        debugCube.GetComponent<BoxCollider>().enabled = false;
+        debugCube.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/debugcube");
+    }
+
+    void Update()
+    {
+        var fastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        var movementSpeed = fastMode ? this.fastMovementSpeed : this.movementSpeed;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + (-transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + (transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + (-transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position = transform.position + (transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position = transform.position + (-transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp))
+        {
+            transform.position = transform.position + (Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown))
+        {
+            transform.position = transform.position + (-Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+
+        //make this a function. i have repeating code. fool.
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Terrain")
+                {
+                    world.GetChunkFromVector3(hit.transform.position).AlterTerrain(false, hit.point);
+                    debugCube.transform.position = new Vector3(Mathf.RoundToInt(hit.point.x), Mathf.RoundToInt(hit.point.y), Mathf.RoundToInt(hit.point.z));
+                }
+
+                GetComponent<LineRenderer>().SetPosition(0, cam.transform.position);
+                GetComponent<LineRenderer>().SetPosition(1, hit.point);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Terrain")
+                {
+                    world.GetChunkFromVector3(hit.transform.position).AlterTerrain(true, hit.point);
+                    debugCube.transform.position = new Vector3(Mathf.RoundToInt(hit.point.x), Mathf.RoundToInt(hit.point.y), Mathf.RoundToInt(hit.point.z));
+                }
+
+                GetComponent<LineRenderer>().SetPosition(0, cam.transform.position);
+                GetComponent<LineRenderer>().SetPosition(1, hit.point);
+            }
+        }
+
+        if (looking)
+        {
+            float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
+            float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
+            transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
+        }
+
+        float axis = Input.GetAxis("Mouse ScrollWheel");
+        if (axis != 0)
+        {
+            var zoomSensitivity = fastMode ? this.fastZoomSensitivity : this.zoomSensitivity;
+            transform.position = transform.position + transform.forward * axis * zoomSensitivity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+    }
+
+    void OnDisable()
+    {
+        StopLooking();
+    }
+
+    /// <summary>
+    /// Enable free looking.
+    /// </summary>
+    public void StartLooking()
+    {
+        looking = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /// <summary>
+    /// Disable free looking.
+    /// </summary>
+    public void StopLooking()
+    {
+        looking = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
